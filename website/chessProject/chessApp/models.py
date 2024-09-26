842900090d1b250cf7bd29e4e3ada2c7c8a061ae
dino424 <115557564+dino424@users.noreply.github.com>
2024-09-26 15:55:54 +0100

    Update models.py
    added comments to explain the basic models im going to make, does not include any of the new features we will add yet

--- website/chessProject/chessApp/models.py
+++ website/chessProject/chessApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Player model: person playing the game
+# Game model: to track game session and link the two players, maage turns and track game status
+# Board model: stores state of the chessboard
+# Piece model: rep individual chess piece on the board
+# Move model: Store history of moves

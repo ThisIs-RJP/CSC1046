3f35d269ab7d2c9ac53319917de04c9a976a79ed
RJ <rjparaiso369@gmail.com>
2024-11-13 18:59:48 +0000

    feat(OOAD-88): Game page
    - Created a new global.css
    	- This is for styling that applies to all pages
    - Standard cleanup
    - Create the game page layout
    	- Created game.css and game.html

--- website/chessProject/chessApp/urls.py
+++ website/chessProject/chessApp/urls.py
@@ -4,10 +4,12 @@ from . import views
 from .forms import *
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('register/', views.UserSignupView.as_view(), name="register"),
-    path('', views.home, name='home'),
-    path('new_game/', views.new_game, name='new_game'),
-    path('join_game/', views.join_game, name='join_game'),
-    path('login/',views.LoginView.as_view(template_name="login.html", authentication_form=UserLoginForm), name="login"),
-    path('logout/', views.logout_user, name='logout'),]
+    path('',           views.home,                                                                             name="home"),
+    path('register/',  views.UserSignupView.as_view(),                                                         name="register"),
+    path('',           views.home,                                                                             name='home'),
+    path('new_game/',  views.new_game,                                                                         name='new_game'),
+    path('join_game/', views.join_game,                                                                        name='join_game'),
+    path('login/',     views.LoginView.as_view(template_name="login.html", authentication_form=UserLoginForm), name="login"),
+    path('logout/',    views.logout_user,                                                                      name='logout'),
+    path('game',       views.game,                                                                             name='game'),
+    ]
